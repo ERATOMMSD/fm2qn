548a834ef98204047d38bb0d328c3363572be4a1
Paolo Arcaini <parcaini@gmail.com>
2018-07-15 13:48:13 +0100

    results

--- fm2qn/src/fmtoqn/cli/FM2QNcli.java
+++ fm2qn/src/fmtoqn/cli/FM2QNcli.java
@@ -3,8 +3,6 @@ package fmtoqn.cli;
 import static org.kohsuke.args4j.ExampleMode.ALL;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
@@ -12,7 +10,6 @@ import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
 import es.us.isa.ChocoReasoner.attributed.AttributedProduct;
-import es.us.isa.FAMA.models.FAMAAttributedfeatureModel.FAMAAttributedFeatureModel;
 import fmtoqn.QueueNetwork;
 import fmtoqn.builder.BuildQNfromFM;
 import fmtoqn.products.OptProdExp;
@@ -39,36 +36,19 @@ public class FM2QNcli {
 
 	public void doMain(String[] args) throws Exception {
 		CmdLineParser parser = new CmdLineParser(this);
-
-		// if you have a wider console, you could increase the value;
-		// here 80 is also the default
 		parser.setUsageWidth(80);
-
 		try {
-			// parse the arguments.
 			parser.parseArgument(args);
-
-			// you can parse additional arguments if you want.
-			// parser.parseArgument("more","args");
-
-			// after parsing arguments, you should check
-			// if enough arguments are given.
-			if (fileName == null)
+			if (fileName == null) {
 				throw new CmdLineException(parser, "No feature model is given");
-
+			}
 		} catch (CmdLineException e) {
-			// if there's a problem in the command line,
-			// you'll get this exception. this will report
-			// an error message.
 			System.err.println(e.getMessage());
 			System.err.println("java -jar FM2QN.jar [options...] fileName");
-			// print the list of available options
 			parser.printUsage(System.err);
 			System.err.println();
 
-			// print option sample. This is useful some time
 			System.err.println("  Example: java -jar FM2QN.jar " + parser.printExample(ALL));
-
 			return;
 		}
 
@@ -102,7 +82,7 @@ public class FM2QNcli {
 			b.saveQn(b.getQnName() + semantics + "_minProd.jsimg");
 			System.out.println("Min product: " + minProd);
 		}
-		if(!minProd && !maxProd) {
+		if (!minProd && !maxProd) {
 			b.saveQn(b.getQnName() + ".jsimg");
 		}
 	}
